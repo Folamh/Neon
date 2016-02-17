e31f4b0aa83c93b55b87179492ebdcc540af5268
leginger1 <seamus.tim@gmail.com>
2016-02-17 22:50:03 +0000

    Starting on base code for map and path classes

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,5 +1,31 @@
 package map;
 
-public class Map {
+import java.util.ArrayList;
+import processing.core.*;
 
+public class Map {
+	
+	String mapName;
+	
+	PImage mapBackground;
+	
+	ArrayList<Path> mapPaths;
+	
+	Map() {
+		
+	}
+	
+	public void loadMap() {
+		
+	}
+	
+	public void saveMap() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	
 }
--- src/map/Path.java
+++ src/map/Path.java
@@ -1,5 +1,54 @@
 package map;
 
-public class Path {
+import java.util.ArrayList;
+import processing.core.*;
 
+//Class for holding paths to for game objects to follow
+public class Path {
+	
+	//Class to hold path points
+	public class PathPoint {
+		PVector pos;
+		
+		PathPoint(int x, int y) {
+			pos.x = x;
+			pos.y = y;
+		}
+	}
+	
+	//ArrayList to hold the path
+	ArrayList<PathPoint> p;
+	
+	int prevPoint, curPoint, nextPoint;
+	
+	Path() {
+		//Initializing the array list
+		p = new ArrayList<PathPoint>();
+		
+		prevPoint = 0;
+		curPoint = 0;
+		nextPoint = 0;
+	}
+	
+	//Adds new point to the end of the path
+	public void addPoint(int x, int y) {
+		
+		PathPoint newPoint = new PathPoint(x,y);
+		p.add(newPoint);
+	}
+	
+	//Removes path point at location x
+	public void remPathPoint(int x) {
+		p.remove(x);
+	}
+	
+	//Clears the path of all path points
+	public void clearPath() {
+		p.clear();
+	}
+	
+	//Returns the path
+	public ArrayList<PathPoint> getPath() {
+		return p;
+	}
 }
