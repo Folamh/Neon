0fb2b21f9c9285eafe9140f742526d6c8adc10c8
leginger1 <seamus.tim@gmail.com>
2016-02-17 12:30:41 +0000

    Added class files and packages that will be needed

---
+++ src/control/MainLoop.java
@@ -0,0 +1,13 @@
+package control;
+/*
+ * Main loop for the game, all game state control will be here
+ */
+
+public class MainLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
