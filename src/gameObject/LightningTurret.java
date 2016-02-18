113d160ef5d32d3c632fba40985976e5ada6ff6c
Folamh <ross.murphy912@gmail.com>
2016-02-18 15:49:57 +0000

    Added all base Tower classes.

---
+++ src/gameObject/LightningTurret.java
@@ -0,0 +1,16 @@
+package gameObject;
+
+public class LightningTurret extends Tower{
+	
+	LightningTurret(int x, int y){
+		super(x, y);
+	}
+	
+	void update(){
+		
+	}
+	
+	void render(){
+		//turret base goes here
+	}
+}
