5ba1709199e99c6c6808cc398c38210f9dde6970
Folamh <ross.murphy912@gmail.com>
2016-02-19 09:55:23 +0000

    Added some lists to tower

--- src/gameObject/Tower.java
+++ src/gameObject/Tower.java
@@ -1,13 +1,21 @@
 package gameObject;
 
+import java.util.*;
+
 import processing.core.*;
 
 public  abstract class Tower extends GameObject{
 	PVector aim;
 	PVector defaultPlane;
+
+	ArrayList<Projectile> projectiles;
+	ArrayList<Enemy> targets;
+	Enemy leadTarget;
 	
 	Tower(int x, int y){
 		super(x, y);
 		defaultPlane = new PVector(0, 0);
+		projectiles = new ArrayList<Projectile>();
+		targets = new ArrayList<Enemy>();//The game loop should check and add to this list
 	}
 }
