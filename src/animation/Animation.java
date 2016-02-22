fc0e61e93628e6b3bff26cd31d22bf724dcf7125
hash <hashdoggie@gmail.com>
2016-02-22 12:50:06 +0000

    Imported appropriate classes Added base code to import images

--- src/animation/Animation.java
+++ src/animation/Animation.java
@@ -1,18 +1,41 @@
 package animation;
 
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import javax.imageio.*;
+
 public class Animation 
 {
-	void loadImages()
+	
+	ArrayList<Image> images = new ArrayList<Image>;
+	public void loadImages()
+	{
+		
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File not found!");
+			e.printStackTrace();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error");
+		}
+	}
+	
+	public void runAnimation()
 	{
 		
 	}
 	
-	void runAnimation()
-	{
-		
-	}
-	
-	void chooseAnimation()
+	public void chooseAnimation()
 	{
 		
 	}
