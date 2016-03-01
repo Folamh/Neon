dbe305f026bc2e15e008aebb94cb1a9735391598
hash <barihaashim@gmail.com>
2016-03-01 14:36:48 +0000

    yerme 2.0

--- src/control/MainLoop.java
+++ src/control/MainLoop.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package control;
 
 import processing.core.*;
@@ -30,59 +30,7 @@ public class MainLoop extends PApplet{
 		frameRate(60);
 		map.loadMap("Resources/Maps/map1.txt");
 		tiles = map.getTiles();
-		Animation animation = new Animation(this, frameCount);
-		animation.loadImages();
-	}
-	
-	public void draw() {
-		background(0);
-		map.render();
-		pushMatrix();
-		for(int i = 0;i<6;i++)
-		{
-			image(animation.basicEnemyMoving[i],250,250);
-			if(i >= 6)
-			{
-				i = 0;
-			}
-		}
-		popMatrix();
-	}
-
-}
-=======
-package control;
-
-import processing.core.*;
-
-import java.util.ArrayList;
-
-import animation.Animation;
-import gameObject.*;
-import map.*;
-
-public class MainLoop extends PApplet{
-	
-	//DO NOT CHANGE THIS UNDER ANY CIRCUMSTANCES!!!!!!!!!
-	public static void main(String[] args) {
-		String[] a = {"MAIN"};			
-		PApplet.runSketch(a, new MainLoop());
-	}
-	
-	//Settings go here
-	public void settings() {
-		size(500,500);
-	}
-	
-	Map map = new Map(this);
-	ArrayList<Tile> tiles;
-	Animation animation;
-	//Only use this for initializing variables
-	public void setup() {
-		frameRate(60);
-		map.loadMap("Resources/Maps/map1.txt");
-		tiles = map.getTiles();
-		animation = new Animation(this, "Resources/Images/Enemy/Basic Enemy/Moving",100);
+		animation = new Animation(this, "Resources/Images/Enemy/Basic Enemy/Moving",1.5);
 		animation.loadImages();
 		
 	}
@@ -96,4 +44,3 @@ public class MainLoop extends PApplet{
 	}
 
 }
->>>>>>> hash
