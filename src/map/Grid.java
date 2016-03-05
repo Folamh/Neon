3d38b3a74925ad157b70ff576ca8d0b54408fc61
hash <barihaashim@gmail.com>
2016-03-05 20:04:14 +0000

    Added more to grid

--- src/map/Grid.java
+++ src/map/Grid.java
@@ -1,21 +1,26 @@
 package map;
 
 import processing.core.*;
-
 public class Grid {
 	
 	PApplet p;
 	int turretLocation;
 	boolean showGrid;
+	PImage gridImage;
 	Grid(int turretLocation, boolean showGrid)
 	{
 		this.turretLocation = turretLocation;
 		this.showGrid = showGrid;
-		
+		if(showGrid)
+		{
+			loadGrid();
+		}
 	}
 	
 	void loadGrid()
 	{
+		p.loadImage("Resources/Images/Grid/GridTemplate.png");
+		p.image(gridImage, p.width/2,0);
 		
 	}
 }
