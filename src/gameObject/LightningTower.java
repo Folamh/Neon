67cfa39885707e8cb2d47cd5d8bdc51e1091be23
hash <barihaashim@gmail.com>
2016-04-19 19:03:25 +0100

    Updated different towers

--- src/gameObject/LightningTower.java
+++ src/gameObject/LightningTower.java
@@ -1,20 +1,71 @@
 package gameObject;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PVector;
 
 public class LightningTower extends Tower{
+	PVector headPoint;
+	float angle;
+	PImage base;
+	PImage head;
 	
 	LightningTower(PApplet p, int x, int y){
 		super(p, x, y);
+		base = p.loadImage("resources/images/turret/basicturret/0.png");
+		head = p.loadImage("resources/images/turret/basicturret/1.png");
+		fireRate = 30;
+		headPoint = new PVector(pos.x, pos.y - 10);
+		range = 250;
+		projectiles = new ArrayList<Projectile>();
+		targets = new ArrayList<Enemy>();//The game loop should check and add to this list
 	}
 	
 	public void update(){
+		if(leadTarget != null){
+			shoot();
+			
+			PVector o = PVector.sub(leadTarget.pos, pos);
+			angle = PApplet.atan2(o.y, o.x) - PApplet.HALF_PI;
+			
+			if(aim.y < 0) angle = - angle;
+			
+		} else {
+			
+			angle = 0;
+			
+		}
 		
+		for(int i = 0; i < projectiles.size(); i++){
+				projectiles.get(i).moveToTarget();
+		}
+		
+		cleanProjectiles();
 	}
 	
 	public void render(){
 		p.pushMatrix();
-		//turret base goes here
+		for(int i = 0; i < projectiles.size(); i++){
+			projectiles.get(i).render();
+		}
+		p.image(base, pos.x, pos.y);
+		
+		p.translate(headPoint.x, headPoint.y);
+		p.rotate(angle);
+		p.image(head, 0, 10);
 		p.popMatrix();
+	}
+	
+	public void shoot(){
+		aim.set(leadTarget.pos);
+		if(shootTimer < fireRate) {
+			shootTimer++; 
+		} else {
+			projectiles.add(new PlasmaProjectile(p, headPoint.x, headPoint.y, leadTarget));
+			shootTimer = 0;
+		}
+		
 	}
 }
